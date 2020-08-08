cf5521a4d0d977ef34f93d7e1578cd4fb7c0926a
Cezar Craciunoiu <craciunoiu.cezar@yahoo.com>
2020-08-08 19:11:41 +0300

    Fixed some coding style inconsistencies

--- plat/xen/balloon.c
+++ plat/xen/balloon.c
@@ -17,73 +17,68 @@
 
 /**
  * set up and call Xen hypercall to ask for memory back from Xen
-*/
+ */
 static int xenmem_reservation_increase(int count, xen_pfn_t *frames, int order)
 {
         struct xen_memory_reservation res = {
 #if __XEN_INTERFACE_VERSION__ >= 0x00030209
-                .memflags = 0;
+		.memflags = 0;
 #else
-                .address_bits = 0,
+		.address_bits = 0,
 #endif
-                .extent_order = order,
-                .domid        = DOMID_SELF
-        };
+		.extent_order = order,
+		.domid        = DOMID_SELF
+	};
 
-        set_xen_guest_handle(res.extent_start, frames);
-        res.nr_extents = count;
+	set_xen_guest_handle(res.extent_start, frames);
+	res.nr_extents = count;
 
-		/* Needs physical frame number */
-        int r = HYPERVISOR_memory_op(XENMEM_populate_physmap, &res);
-        return r;
+	/* Needs physical frame number */
+	return HYPERVISOR_memory_op(XENMEM_populate_physmap, &res);
 }
 
 /**
  * set up and call Xen hypercall to give memory to Xen
-*/
+ */
 static int xenmem_reservation_decrease(int count, xen_pfn_t *frames, int order)
 {
-        struct xen_memory_reservation res = {
+	struct xen_memory_reservation res = {
 #if __XEN_INTERFACE_VERSION__ >= 0x00030209
-                .mem_flags = 0,
+		.mem_flags = 0,
 #else
-                .address_bits = 0,
+		.address_bits = 0,
 #endif
-                .extent_order = order,
-                .domid        = DOMID_SELF
-        };
+		.extent_order = order,
+		.domid        = DOMID_SELF
+	};
 
-        set_xen_guest_handle(res.extent_start, frames);
-        res.nr_extents = count;
+	set_xen_guest_handle(res.extent_start, frames);
+	res.nr_extents = count;
 
-		/* Needs guest frame number */
-        int r = HYPERVISOR_memory_op(XENMEM_decrease_reservation, &res);
-        return r;
+	/* Needs guest frame number */
+	return HYPERVISOR_memory_op(XENMEM_decrease_reservation, &res);
 }
 
 /**
  * When we inflate we will be decreasing the memory available to the VM
  * We will give the extent of extent order = order starting at va to the host.
-*/
+ */
 int ukplat_inflate(void* va, int order)
 {
-
 	xen_pfn_t pfn = virt_to_pfn(va);
-	int r = xenmem_reservation_decrease(1, &pfn, order);
 
-	return r;
+	return xenmem_reservation_decrease(1, &pfn, order);
 }
 
 /**
  * When we deflate we will be increasing the memory available to the VM
- * We will ask for 1 extent of extent order = order back from the host. It will map
- * The extent to the address va.
-*/
+ * We will ask for 1 extent of extent order = order back from the host.
+ * It will map the extent to the address va.
+ */
 int ukplat_deflate(void* va, int order)
 {
 	/* Make sure we are sending the correct frame number. Should be a GFN */
 	//xen_pfn_t pfn = virt_to_pfn(va);
-	int r = xenmem_reservation_increase(1, &va, order);
 
-	return r;
+	return xenmem_reservation_increase(1, &va, order);
 }
