94bca539bc9dab47259deaa5f32c750826c7d7d2
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2020-08-26 11:58:25 +0300

    Added header. Modified some comments. Removed redundant content.

--- plat/drivers/balloon/balloon_drv.c
+++ plat/drivers/balloon/balloon_drv.c
@@ -1,4 +1,38 @@
-
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Cason Schindler & Jack Raney <cason.j.schindler@gmail.com>
+ *          Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
+ *
+ * Copyright (c) 2019, The University of Texas at Austin. All rights reserved.
+ *               2020, University Politehnica of Bucharest. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
 #include <uk/plat/common/sections.h>
 #include <sys/types.h>
 #include <uk/assert.h>
@@ -24,7 +58,7 @@ static struct virtio_balloon_device *global_vb;
 /* pages given to hypervisor (in the balloon) */
 struct balloon_pages {
 
-	uint32_t num_pages; /* # pages in balloon */
+	uint32_t num_pages;
 
 };
 
@@ -33,7 +67,7 @@ struct balloon_pages {
  */
 struct transport_pages {
 
-	uint32_t num_pages; /* # of pages in pages */
+	uint32_t num_pages;
 	uint32_t *pages;
 
 };
@@ -112,13 +146,14 @@ static void vtballoon_send_page_frames(struct virtio_balloon_device *vb,
 	virtqueue_host_notify(vq);
 
 	/* wait on KVM to respond. Need a safer method for this */
-	while ((c = virtqueue_buffer_dequeue(vq, &vq_cookie, &len)) < 0);
+	while ((c = virtqueue_buffer_dequeue(vq, &vq_cookie, &len)) < 0)
+		;
 
 }
 
 /**
- * this is equivalent to leaking from the balloon and
- * increasing memory reservation for guest
+ * This is equivalent to leaking from the balloon and
+ * increasing memory reservation for guest.
  */
 int deflate_balloon(uintptr_t *pages_to_guest, uint32_t num)
 {
@@ -138,7 +173,6 @@ int deflate_balloon(uintptr_t *pages_to_guest, uint32_t num)
 		num = vb->balloon->num_pages;
 
 	for (i = 0; i < num; i++) {
-		 /* put page in temp array for host */
 		vb->transport->pages[i] = pages_to_guest[i];
 		vb->balloon->num_pages -= 1;
 		vb->transport->num_pages += 1;
@@ -157,8 +191,8 @@ int deflate_balloon(uintptr_t *pages_to_guest, uint32_t num)
 }
 
 /**
- * this is equivalent to filling the balloon and
- * decreasing memory reservation for guest
+ * This is equivalent to filling the balloon and
+ * decreasing memory reservation for guest.
  */
 int inflate_balloon(uintptr_t *pages_to_host, uint32_t num)
 {
@@ -175,7 +209,6 @@ int inflate_balloon(uintptr_t *pages_to_host, uint32_t num)
 	clear_transport(vb);
 
 	for (i = 0; i < num; i++) {
-		/* put page in temp array for host */
 		vb->transport->pages[i] = pages_to_host[i] / __PAGE_SIZE;
 		vb->balloon->num_pages += 1;
 		vb->transport->num_pages += 1;
@@ -244,8 +277,7 @@ exit:
 
 static int virtio_balloon_start(struct virtio_balloon_device *vb)
 {
-	virtqueue_intr_enable(vb->inflate_vq);
-	virtqueue_intr_enable(vb->deflate_vq);
+	/* Disable interrupts for queues to suppress error messages */
 	virtqueue_intr_disable(vb->inflate_vq);
 	virtqueue_intr_disable(vb->deflate_vq);
 	virtio_dev_drv_up(vb->vdev);
@@ -259,7 +291,6 @@ static int virtio_balloon_add_dev(struct virtio_dev *vdev)
 
 	struct virtio_balloon_device *vbdev;
 	int rc = 0;
-	int tag_len = 30;
 	void *alc;
 
 	UK_ASSERT(vdev != NULL);
@@ -271,7 +302,6 @@ static int virtio_balloon_add_dev(struct virtio_dev *vdev)
 		goto err_out;
 	}
 
-	vbdev->tag = uk_calloc(a, 1, sizeof(tag_len));
 	vbdev->tag = "VIRTIO_BALLOON_DRV_DEV";
 
 	uk_mutex_init(&vbdev->lock);
@@ -304,10 +334,9 @@ static int virtio_balloon_add_dev(struct virtio_dev *vdev)
 		goto err_out;
 
 exit:
-	global_vb = vbdev; /* initialize global vb */
+	global_vb = vbdev;
 	/* initial alloc and free to trigger ballon init */
 	alc = uk_palloc(a, 1);
-
 	uk_pfree(a, alc, 1);
 	return rc;
 err_out:
@@ -321,7 +350,6 @@ err_out:
 
 static int virtio_balloon_drv_init(struct uk_alloc *drv_allocator)
 {
-	/* driver initialization */
 	if (!drv_allocator)
 		return -EINVAL;
 
