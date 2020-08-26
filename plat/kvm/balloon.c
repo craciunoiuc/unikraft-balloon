c262c358cca41652f3e60af6563a3c61e26f7047
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2020-08-26 12:24:10 +0300

    Added header. Added function to define behaviour outside of platform specific sources.

--- plat/kvm/balloon.c
+++ plat/kvm/balloon.c
@@ -1,11 +1,43 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Cason Schindler & Jack Raney <cason.j.schindler@gmail.com>
+ *          Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
+ *
+ * Copyright (c) 2019, The University of Texas at Austin. All rights reserved.
+ *               2020, University Politehnica of Bucharest. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
 #include <inttypes.h>
 #include <balloon/balloon.h>
 #include <uk/plat/balloon.h>
 #include <uk/asm/limits.h>
 
-/**
- * number of pages is 2^order
- */
 static inline int get_num_pages(int order)
 {
 	int num_pages = 1;
@@ -17,7 +49,7 @@ static inline int get_num_pages(int order)
 }
 
 /**
- * fill addresses for page range starting at first_page
+ * Fills addresses for page range starting at first_page.
  */
 static inline void fill_page_array(uintptr_t *pages_array,
 			void *first_page, int num_pages)
@@ -32,7 +64,7 @@ static inline void fill_page_array(uintptr_t *pages_array,
 }
 
 /**
- * call driver inflate_balloon
+ * Calls driver inflate_balloon
  * returns number of pages actually put into balloon or < 0 on error
  */
 int ukplat_inflate(void *page, int order)
@@ -45,7 +77,7 @@ int ukplat_inflate(void *page, int order)
 }
 
 /**
- * call driver deflate_balloon
+ * Call driver deflate_balloon
  * returns number of pages actually taken from balloon or < 0 on error
  */
 int ukplat_deflate(void *page, int order)
@@ -55,4 +87,11 @@ int ukplat_deflate(void *page, int order)
 
 	fill_page_array(pages_to_guest, page, num_pages);
 	return deflate_balloon(pages_to_guest, num_pages);
+}
+
+/**
+ * Sets behaviour for KVM.
+ */
+void ukplat_balloon_set(char *balloon_type) {
+	*balloon_type = 2;
 }
--- plat/xen/balloon.c
+++ plat/xen/balloon.c
@@ -1,3 +1,38 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Authors: Cason Schindler & Jack Raney <cason.j.schindler@gmail.com>
+ *          Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
+ *
+ * Copyright (c) 2019, The University of Texas at Austin. All rights reserved.
+ *               2020, University Politehnica of Bucharest. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
+ */
 #include <uk/plat/common/sections.h>
 
 #include <common/gnttab.h>
@@ -10,13 +45,12 @@
 #include <xen-arm/mm.h>
 #endif
 
-
 #include <xen/memory.h>
 #include <uk/plat/balloon.h>
 #include <common/hypervisor.h>
 
 /**
- * set up and call Xen hypercall to ask for memory back from Xen
+ * Set up and call Xen hypercall to ask for memory back from Xen.
  */
 static int xenmem_reservation_increase(int count, xen_pfn_t *frames, int order)
 {
@@ -38,7 +72,7 @@ static int xenmem_reservation_increase(int count, xen_pfn_t *frames, int order)
 }
 
 /**
- * set up and call Xen hypercall to give memory to Xen
+ * Set up and call Xen hypercall to give memory to Xen.
  */
 static int xenmem_reservation_decrease(int count, xen_pfn_t *frames, int order)
 {
@@ -81,4 +115,11 @@ int ukplat_deflate(void *va, int order)
 	xen_pfn_t pfn = virt_to_pfn(va);
 
 	return xenmem_reservation_increase(1, &pfn, order);
+}
+
+/**
+ * Sets behaviour for Xen.
+ */
+void ukplat_balloon_set(char *balloon_type) {
+	*balloon_type = 0;
 }
